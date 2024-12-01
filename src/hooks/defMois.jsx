881189e7872dcee7717bc6182dc7dc889631e6f1
clément4444 <clement.fiquet0@gmail.com>
2024-12-01 13:20:27 +0100

    bug mois accent

--- src/hooks/defMois.jsx
+++ src/hooks/defMois.jsx
@@ -1,7 +1,7 @@
 export default function getMois(moisIndex) {
     const mois = [
-        "janvier", "février", "mars", "avril", "mai", "juin",
-        "juillet", "août", "septembre", "octobre", "novembre", "décembre"
+        "janvier", "fevrier", "mars", "avril", "mai", "juin",
+        "juillet", "aout", "septembre", "octobre", "novembre", "decembre"
     ];
     return mois[moisIndex - 1]; // `moisIndex - 1` car les mois commencent à 1 (janvier = 1)
 }
